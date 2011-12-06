d1a8860ceb87a60a4c63250c9fdaf57a28686a02
darnold <darnold@derek-webspec>
2011-12-06 13:30:37 -0600

    SSH arg is now SCP arg. dontwatch works now.

--- watchthis.sh
+++ watchthis.sh
@@ -2,19 +2,31 @@
 
 _watchthis_incronflags="IN_MODIFY,IN_CLOSE_WRITE,IN_CREATE,IN_MOVED_TO,IN_ATTRIB"
 
+_watchthis_spool=/var/spool/incron/`whoami`
+
 watchthis()
 {
+    # need to remove previous entries to do an update
+    dontwatch
     thisdir=$(pwd | sed -e 's/ /\\ /g')
     SAVEIFS=$IFS
     IFS=$(echo -en "\n\b")
-    dirs=$(find `pwd` -type d -print)
+    dirs=$(find `pwd` -type d -print | sort | uniq)
     for dir in $dirs; do
         edir=$(echo $dir | sed -e 's/ /\\ /g')
-        echo "Adding $edir to incrontab."
-        echo "$edir $_watchthis_incronflags /usr/bin/rsync -atz $thisdir/ $1:~/">>/var/spool/incron/darnold
+        echo "$edir $_watchthis_incronflags /usr/bin/rsync -atz $thisdir/ $1">>$_watchthis_spool
     done
     IFS=$SAVEIFS
     incrontab --reload
 }
 
-complete -F _ssh watchthis
+dontwatch()
+{
+    thisdir=$(pwd | sed -e 's/ /\\\\ /g')
+    tmpfile=/tmp/`whoami`_watchthis
+    grep -v "atz $thisdir" $_watchthis_spool>$tmpfile
+    cp $tmpfile $_watchthis_spool
+    incrontab --reload
+}
+
+complete -o nospace -F _scp watchthis
