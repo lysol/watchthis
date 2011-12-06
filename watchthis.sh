73322a4c648b0542193fdb13d5b7ec11b01cf439
Derek Arnold <github@vyr.us>
2011-12-06 16:54:20 -0600

    Added a mutex

--- watchthis.sh
+++ watchthis.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 _watchthis_incronflags="IN_MODIFY,IN_CLOSE_WRITE,IN_CREATE,IN_MOVED_TO,IN_ATTRIB"
-
 _watchthis_spool=/var/spool/incron/`whoami`
+_lockfile=$HOME/.watchthis-lock
+
+# set up the sync script
+if [ ! -d $HOME/bin ]; then
+    mkdir $HOME/bin
+fi
+
+echo "#!/bin/bash
+if [ ! -f $_lockfile ]; then
+    touch $_lockfile
+    echo \"Syncing \$1 to \$2\" >>$HOME/.watchthis.log
+    sleep 1
+    /usr/bin/env rsync -atz \"\$1\" \"\$2\" >>$HOME/.watchthis.log
+    rm $_lockfile
+fi
+">$HOME/bin/dosync.sh
+chmod +x $HOME/bin/dosync.sh
 
 watchthis()
 {
@@ -10,23 +26,30 @@ watchthis()
     dontwatch
     thisdir=$(pwd | sed -e 's/ /\\ /g')
     SAVEIFS=$IFS
+    tmpfile=/tmp/`whoami`_incrontab
+    if [ -f $tmpfile ]; then
+        rm $tmpfile
+    fi
     IFS=$(echo -en "\n\b")
     dirs=$(find `pwd` -type d -print | sort | uniq)
     for dir in $dirs; do
         edir=$(echo $dir | sed -e 's/ /\\ /g')
-        echo "$edir $_watchthis_incronflags /usr/bin/rsync -atz $thisdir/ $1">>$_watchthis_spool
+        echo "processing $dir"
+        echo "$edir $_watchthis_incronflags $HOME/bin/dosync.sh $thisdir/ $1">>$tmpfile
     done
     IFS=$SAVEIFS
-    incrontab --reload
+    echo "adding"
+    cat $tmpfile >> $_watchthis_spool
+    rm $_lockfile
+    return 0
 }
 
 dontwatch()
 {
     thisdir=$(pwd | sed -e 's/ /\\\\ /g')
     tmpfile=/tmp/`whoami`_watchthis
-    grep -v "atz $thisdir" $_watchthis_spool>$tmpfile
+    grep -v "dosync.sh $thisdir" $_watchthis_spool>$tmpfile
     cp $tmpfile $_watchthis_spool
-    incrontab --reload
 }
 
 complete -o nospace -F _rsync watchthis
